55622e553fb71dc4c99510117959192f80b12722
kansei-bit <kansei1001@gmail.com>
2020-10-15 13:17:31 +0900

    コメント機能などの実装

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,4 +1,7 @@
 class PrototypesController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :move_to_index, only: :edit
+
   def index
     @prototypes = Prototype.all
   end
@@ -19,10 +22,11 @@ class PrototypesController < ApplicationController
 
   def show
     @prototype = Prototype.find(params[:id])
+    @comment = Comment.new
+    @comments = @prototype.comments.includes(:user)
   end
 
   def edit
-    @prototype = Prototype.find(params[:id])
   end
 
   def update
@@ -43,5 +47,13 @@ class PrototypesController < ApplicationController
   private
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    @prototype = Prototype.find(params[:id])
+    @user = @prototype.user
+    unless @user == current_user
+      redirect_to action: :index
+    end
   end
 end
